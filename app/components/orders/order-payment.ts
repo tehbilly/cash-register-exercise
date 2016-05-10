e256a9ad1d24fe0e3f879f926d86c063c64ab835
William McGann <contact@WilliamMcGann.com>
2016-05-10 15:06:41 -0400

    Start tracking amount tendered

--- app/components/orders/order-payment.ts
+++ app/components/orders/order-payment.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { OrderService } from './order-service';
 
 @Component({
-  template: require('./order-payment.html')
+  template: require('./order-payment.html'),
+  directives: [
+    ROUTER_DIRECTIVES
+  ]
 })
-export class OrderPaymentComponent {}
+export class OrderPaymentComponent {
+  amountTendered: number = 0.00;
+  constructor(private orderService: OrderService) {}
+}
